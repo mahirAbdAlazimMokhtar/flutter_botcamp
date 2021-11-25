29de3648f305b6decf380b2e095beb2dda0c2ba0
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-11-25 19:38:12 +0200

    add create and insert to db

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 import 'bmi_result_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -12,12 +13,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double? result;
-  bool isMale = true ;
+  bool isMale = true;
   double height = 80;
   int age = 19;
 
   int weight = 5;
-
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           children: [
                             Image.asset(
                               'images/female.png',
-                              width : 90,
+                              width: 90,
                               height: 90,
                             ),
                             const SizedBox(
@@ -109,46 +109,46 @@ class _HomeScreenState extends State<HomeScreen> {
           // this for height by cm
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey[400]),
-                  child: Column(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[400]),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'HEIGHT',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    textBaseline: TextBaseline.alphabetic,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text(
-                        'HEIGHT',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Text(
+                        '${height.round()}',
+                        style: const TextStyle(fontSize: 25),
                       ),
-                      Row(
-                        textBaseline: TextBaseline.alphabetic,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '${height.round()}',
-                            style:const TextStyle(fontSize: 25),
-                          ),
-                          const Text('CM'),
-                        ],
-                      ),
-                      Slider(
-                          value: height,
-                          max: 220,
-                          min: 80,
-                          onChanged: (value) {
-                            setState(() {
-                              height = value;
-                            });
-                          })
+                      const Text('CM'),
                     ],
                   ),
-                ),
-              )),
+                  Slider(
+                      value: height,
+                      max: 220,
+                      min: 80,
+                      onChanged: (value) {
+                        setState(() {
+                          height = value;
+                        });
+                      })
+                ],
+              ),
+            ),
+          )),
           //this for spacing between items
           const SizedBox(
             height: 15.0,
@@ -156,120 +156,122 @@ class _HomeScreenState extends State<HomeScreen> {
           //this for weight and age
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[400]),
-                        child: Column(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey[400]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '$weight',
+                          style: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const Text(
-                              'WEIGHT',
-                              style: TextStyle(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            FloatingActionButton(
+                              mini: true,
+                              heroTag: 'weight-',
+                              onPressed: () {
+                                setState(() {
+                                  if (weight > 1) weight--;
+                                });
+                              },
+                              child: const Icon(
+                                Icons.remove,
+                              ),
                             ),
-                            Text(
-                              '$weight',
-                              style: const TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            FloatingActionButton(
+                              heroTag: 'weight+',
+                              mini: true,
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              child: const Icon(
+                                Icons.add,
+                              ),
                             ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FloatingActionButton(
-                                  mini: true,
-                                  heroTag: 'weight-',
-                                  onPressed: () {
-                                    setState(() {
-                                      if (weight > 1) weight--;
-                                    });
-                                  },
-                                  child: const Icon(
-                                    Icons.remove,
-                                  ),
-                                ),
-                                FloatingActionButton(
-                                  heroTag: 'weight+',
-                                  mini: true,
-                                  onPressed: () {
-                                    setState(() {
-                                      weight++;
-                                    });
-                                  },
-                                  child: const Icon(
-                                    Icons.add,
-                                  ),
-                                ),
-                              ],
-                            )
                           ],
-                        ),
-                      ),
+                        )
+                      ],
                     ),
-                    const SizedBox(
-                      width: 15.0,
-                    ),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey[400]),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'AGE',
-                              style: TextStyle(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              '$age',
-                              style: const TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FloatingActionButton(
-                                  mini: true,
-                                  heroTag: 'age-',
-                                  onPressed: () {
-                                    setState(() {
-                                      age--;
-                                    });
-                                  },
-                                  child: const Icon(
-                                    Icons.remove,
-                                  ),
-                                ),
-                                FloatingActionButton(
-                                  heroTag: 'age+',
-                                  mini: true,
-                                  onPressed: () {
-                                    setState(() {
-                                      age++;
-                                    },);
-                                  },
-                                  child: const Icon(
-                                    Icons.add,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              )),
+                const SizedBox(
+                  width: 15.0,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey[400]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'AGE',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '$age',
+                          style: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              mini: true,
+                              heroTag: 'age-',
+                              onPressed: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                              child: const Icon(
+                                Icons.remove,
+                              ),
+                            ),
+                            FloatingActionButton(
+                              heroTag: 'age+',
+                              mini: true,
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    age++;
+                                  },
+                                );
+                              },
+                              child: const Icon(
+                                Icons.add,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
           //this for button
-           Container(
+          SizedBox(
             width: double.infinity,
             child: MaterialButton(
               onPressed: () {
@@ -279,7 +281,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => BMIRESULT(age: age,isMale:isMale ,result:result.round() ),
+                    builder: (context) => BMIRESULT(
+                        age: age, isMale: isMale, result: result.round()),
                   ),
                 );
               },
@@ -292,4 +295,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
