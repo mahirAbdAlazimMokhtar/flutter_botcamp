44913e731e626d050638b7fdbf988a3c73fc908c
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-04 16:22:07 +0200

    change main page last code

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _BmiScreenState extends State<BmiScreen> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeLayout(),
+      home: TodoApp(),
     );
   }
 }
