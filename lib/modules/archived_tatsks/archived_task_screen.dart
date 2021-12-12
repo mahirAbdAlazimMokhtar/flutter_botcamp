0d218e35c2bca749db47e3855ea9f702295f0771
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-12 17:50:22 +0200

    add cubit to archived tasks

--- lib/modules/archived_tatsks/archived_task_screen.dart
+++ lib/modules/archived_tatsks/archived_task_screen.dart
@@ -1,10 +1,54 @@
+import 'package:botcamp_flutter_challenge/components/components.dart';
+import 'package:botcamp_flutter_challenge/shared/cubit/cubit.dart';
+import 'package:botcamp_flutter_challenge/shared/cubit/states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ArchivedTaskScreen extends StatelessWidget {
   const ArchivedTaskScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, states) {},
+      builder: (context, states) {
+        var tasks = AppCubit.get(context).archivedTasks;
+        return tasks.isNotEmpty
+            ? ListView.separated(
+                itemBuilder: (context, index) =>
+                    buildTaskItem(tasks[index], context),
+                separatorBuilder: (context, index) => Padding(
+                      padding: const EdgeInsetsDirectional.only(start: 20.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 1.0,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                itemCount: tasks.length)
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.menu,
+                      size: 100.0,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'No Archived Tasks Yet, Plz add new tasks',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              );
+      },
+    );
   }
 }
