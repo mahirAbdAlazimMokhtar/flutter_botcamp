0bbedd592d3b2ca85a8a33f4e17c697df420fb6b
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-12 17:57:17 +0200

    add states cubit to project

---
+++ lib/shared/cubit/states.dart
@@ -0,0 +1,21 @@
+abstract class AppStates {}
+
+class AppInitialState extends AppStates {}
+
+class AppChangeBottomNavBarState extends AppStates {}
+
+class AppCreateDatabaseState extends AppStates {}
+
+class AppGetDatabaseLoadingState extends AppStates {}
+
+class AppUpdateDatabaseLoadingState extends AppStates {}
+
+class AppUpdateDatabaseState extends AppStates {}
+
+class AppDeleteDatabaseState extends AppStates {}
+
+class AppInsertDatabaseState extends AppStates {}
+
+class AppGetDatabaseState extends AppStates {}
+
+class AppChangeBottomSheetState extends AppStates {}
